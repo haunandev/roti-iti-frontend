440be812f6cf88e300d19998845f48ecec7ee658
iyasmzn <58409100+iyasmzn@users.noreply.github.com>
2023-09-07 22:46:24 +0700

    update landingpage

--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -7,13 +7,13 @@
         </a>
         <ul class="navbar-nav">
           <li class="nav-item">
-            <a @click="scrollTo('#services')" href="javascript:;">Beranda</a>
+            <a @click="scrollTo('#home')" href="javascript:;">Beranda</a>
           </li>
           <li class="nav-item">
-            <a @click="scrollTo('#features')" href="javascript:;">Produk</a>
+            <a @click="scrollTo('#product')" href="javascript:;">Produk</a>
           </li>
           <li class="nav-item">
-            <a @click="scrollTo('#features')" href="javascript:;">Toko Kami</a>
+            <a @click="scrollTo('#aboutUs')" href="javascript:;">Toko Kami</a>
           </li>
           <li class="nav-item">
             <div class="separator"></div>
@@ -40,24 +40,33 @@
                 <div class="w-100 d-flex align-items-center justify-content-start position-relative">
                   <ul class="navbar-nav d-none d-lg-flex flex-row justify-content-center w-100">
                     <li class="nav-item">
-                      <a @click="scrollTo('#services')" class="text-dark" href="javascript:;">Beranda</a>
+                      <a @click="scrollTo('#home')" class="text-dark" href="javascript:;">Beranda</a>
                     </li>
                     <li class="nav-item">
-                      <a @click="scrollTo('#features')" class="text-dark" href="javascript:;">Produk</a>
+                      <a @click="scrollTo('#product')" class="text-dark" href="javascript:;">Produk</a>
                     </li>
                     <li class="nav-item">
-                      <a @click="scrollTo('#features')" class="text-dark" href="javascript:;">Toko Kami</a>
+                      <a @click="scrollTo('#aboutUs')" class="text-dark" href="javascript:;">Toko Kami</a>
                     </li>
                     <li class="nav-item">
                       <div class="separator"></div>
                     </li>
                   </ul>
-                  <div class="">
+                  <div class="d-flex align-items-center">
                     <a
-                      class="btn btn-primary text-white btn-sm mobile-menu-cta"
+                      class="mobile-menu-cta mr-3"
                       target="_blank"
                       :href="contactUrl"
-                    >S</a>
+                    >
+                      <i class="simple-icon-user"></i>
+                    </a>
+                    <a
+                      class="mobile-menu-cta"
+                      target="_blank"
+                      :href="contactUrl"
+                    >
+                      <i class="simple-icon-handbag"></i>
+                    </a>
                   </div>
                 </div>
                 <span
@@ -72,7 +81,8 @@
         </div>
 
         <div class="content-container" id="home">
-          <div class="section bg-theme-3" id="sectionHome">
+          <div class="section bg-theme-3" id="sectionHome" style="position: relative;">
+            <img class="hero-lg-image" data-aos="zoom-in" data-aos-delay="250" alt="hero" src="/assets/img/landing-page/home-hero.png"/>
             <div class="container">
               <div class="row home-row" id="homeRow">
                 <div class="col-12 d-block d-md-none">
@@ -85,30 +95,27 @@
                   </a>
                 </div>
 
-                <div class="col-12 col-xl-7 offset-xl-1 col-lg-7 col-md-6 d-none d-md-block">
-                  <a href="#">
-                    <img data-aos="zoom-in" data-aos-delay="250" alt="hero" src="/assets/img/landing-page/home-hero.png" style="height: 500px;" />
-                  </a>
+                <div class="col-12 col-xl-7 offset-xl-1 col-lg-7 col-md-6 d-none d-md-block" style="height: 500px;">
                 </div>
-                <div class="col-12 col-xl-4 col-lg-5 col-md-6">
+                <div class="col-12 col-xl-4 col-lg-5 col-md-6 d-flex align-items-center justify-content-end">
                   <div class="text-right">
                     <div class="display-1 text-dark mb-2" data-aos="zoom-in">
                       {{ $t('landing-page.title.main') }}
                     </div>
-                    <h3 class="mb-5 text-dark" data-aos="fade-down" data-aos-delay="100">                      
+                    <h2 class="mb-5 text-dark" data-aos="fade-down" data-aos-delay="100">                      
                       {{$t('landing-page.title.desc1')}}
-                    </h3>
-                    <a class="btn btn-outline-dark btn-xl mr-2 mb-2" data-aos="fade-down" data-aos-delay="150" @click="scrollTo('#services')">
+                    </h2>
+                    <b-button variant="outline-dark" class="mr-2 mb-2 rounded-lg" data-aos="fade-down" data-aos-delay="150" @click="scrollTo('#product')">
                       Belanja Sekarang
-                    </a>
+                    </b-button>
                   </div>
                 </div>
               </div>
 
               <div class="row">
                 <a
-                  class="btn btn-circle btn-outline-semi-light hero-circle-button"
-                  @click="scrollTo('#features')"
+                  class="btn btn-circle btn-outline-primary hero-circle-button"
+                  @click="scrollTo('#product')"
                 >
                   <i class="simple-icon-arrow-down"></i>
                 </a>
@@ -121,103 +128,126 @@
               <div class="home-carousel text-center" data-aos="zoom-in">
                 <h1 class="">Produk</h1>
                 <glide-component :settings="slideSettings">
-                  <div class="card rounded" v-for="(f,index) in slideItems" :key="`slide_${index}`" data-aos="zoom-in" :data-aos-delay="index * 100">
-                    <div class="card-body text-center">
+                  <div class="card" v-for="(f,index) in slideItems" :key="`slide_${index}`" data-aos="zoom-in" :data-aos-delay="index * 100">
+                    <div class="card-body">
                       <div>
-                        <i :class="f.icon+ ' large-icon'"></i>
-                        <h5 class="mb-3 font-weight-semibold">{{f.title}}</h5>
+                        <img src="/assets/img/landing-page/sample/product.png" alt="" class="w-100">
                       </div>
                       <div>
-                        <p class="detail-text">{{f.detail}}</p>
+                        <p class="mt-2 mb-0 text-dark text-left">Roti Stroberi</p>
+                        <p class="text-dark mb-0 text-left">Rp, 3.500</p>
                       </div>
                     </div>
                   </div>
                 </glide-component>
+                <a href="#product" class="d-inline-block mt-4">
+                  Belanja Disini
+                  <i class="simple-icon-arrow-right-circle"></i>
+                </a>
               </div>
             </div>
           </div>
 
           <div class="section">
-            <div class="container" id="features">
-
-              <div v-for="(feature,i) in features" :key="`feature_${i}`">
-                <div v-if="i%2==0" class="row feature-row">
-                  <div class="col-12 col-md-6 col-lg-5 d-flex align-items-center">
-                    <div class="feature-text-container">
-                      <h2>{{feature.title}}</h2>
-                      <p v-html="feature.detail"></p>
-                    </div>
-                  </div>
-                  <div class="col-12 col-md-6 col-lg-6 offset-lg-1 offset-md-0 position-relative">
-                    <img
-                      :alt="feature.title"
-                      :src="feature.img"
-                      class="feature-image-right feature-image-charts position-relative"
-                    />
-                  </div>
+            <div class="container" id="aboutUs">
+              <div class="row feature-row">
+                <div class="col-12 col-md-6 col-lg-6 order-2 order-md-1">
+                  <img
+                    data-aos="zoom-in"
+                    :alt="`Tentang Kami`"
+                    src="/assets/img/landing-page/sample/about-us.png"
+                    class="feature-image-left feature-image-charts w-100"
+                  />
                 </div>
-                <div v-if="i%2==1" class="row feature-row">
-                  <div class="col-12 col-md-6 col-lg-6 order-2 order-md-1">
-                    <img
-                      :alt="feature.title"
-                      :src="feature.img"
-                      class="feature-image-left feature-image-charts"
-                    />
-                  </div>
-                  <div
-                    class="col-12 col-md-6 offset-md-0 col-lg-5 offset-lg-1 d-flex align-items-center order-1 order-md-2"
-                  >
-                    <div class="feature-text-container">
-                      <h2>{{feature.title}}</h2>
-                      <p v-html="feature.detail"></p>
-                    </div>
+                <div
+                  class="col-12 col-md-6 offset-md-0 col-lg-5 offset-lg-1 d-flex align-items-start order-1 order-md-2"
+                >
+                  <div class="feature-text-container">
+                    <h2 data-aos="fade-up">Tentang Kami</h2>
+                    <p data-aos="fade-up">
+                      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed pellentesque odio sit amet odio commodo, at elementum ante congue. Integer ut nunc ut orci facilisis laoreet quis eget nunc. Vivamus laoreet, dolor in viverra bibendum, mi lorem mattis elit, nec vulputate eros magna in odio. Nunc et suscipit massa. Curabitur gravida lectus at nibh commodo convallis. Cras velit risus, pharetra quis vestibulum et, mollis in felis. In ornare finibus quam, ac pellentesque arcu lobortis vel. Suspendisse porttitor, augue in consectetur maximus, nisi ex hendrerit tortor, ut dapibus tellus dolor at lectus.
+                    </p>
+                    <a href="#aboutUs" class="d-inline-block" data-aos="fade-right" data-aos-delay="200">Lebih Lanjut <i class="simple-icon-arrow-right-circle"></i></a>
                   </div>
                 </div>
               </div>
             </div>
           </div>
+          <div class="section">
+            <div class="container" id="review">
+              <h2 data-aos="fade-up">Ulasan</h2>
+              <p class="text-right" data-aos="zoom-in" data-aos-delay="200"> <i class="simple-icon-arrow-left"></i> 1/10 <i class="simple-icon-arrow-right"></i></p>
+              <b-row>
+                <b-col cols="12" lg="6">
+                  <b-card no-body class="overflow-hidden" data-aos="zoom-in">
+                    <b-row no-gutters>
+                      <b-col md="6">
+                        <b-card-img src="/assets/img/landing-page/sample/ulasan.png" alt="Image" class="rounded-0" style="max-height: 301px;"></b-card-img>
+                      </b-col>
+                      <b-col md="6">
+                        <b-card-body title="Anna">
+                          <b-card-text>
+                            <p>Membeli Roti Stoberi</p>
+                            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed pellentesque odio sit amet odio commodo, at elementum ante congue.
+                          </b-card-text>
+                        </b-card-body>
+                      </b-col>
+                    </b-row>
+                  </b-card>
+                </b-col>
+                <b-col cols="12" lg="6">
+                  <b-card no-body class="overflow-hidden" data-aos="zoom-in" data-aos-delay="100">
+                    <b-row no-gutters>
+                      <b-col md="6">
+                        <b-card-img src="/assets/img/landing-page/sample/ulasan.png" alt="Image" class="rounded-0" style="max-height: 301px;"></b-card-img>
+                      </b-col>
+                      <b-col md="6">
+                        <b-card-body title="Anna">
+                          <b-card-text>
+                            <p>Membeli Roti Stoberi</p>
+                            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed pellentesque odio sit amet odio commodo, at elementum ante congue.
+                          </b-card-text>
+                        </b-card-body>
+                      </b-col>
+                    </b-row>
+                  </b-card>
+                </b-col>
+              </b-row>
+            </div>
+          </div> 
 
-          <div class="section background background-no-bottom mb-0 pb-0">
+          <div class="section">
             <div class="container">
-              <div class="row">
-                <div class="col-12 offset-0 col-lg-8 offset-lg-2 text-center">
-                  <h1>Interested?</h1>
-                  <p>Send message by WhatsApp to us if you interested and want to use our services.</p>
-                </div>
-                <div class="col-12 offset-0 col-lg-6 offset-lg-3 newsletter-input-container">
-                  <div class="text-center mb-3">
-                    <a class="btn btn-secondary btn-xl" target="_blank" :href="contactUrl"><b-icon icon="chat" class="mr-1"></b-icon> CONTACT NOW!</a>
-                  </div>
-                </div>
-              </div>
+              <b-row>
+                <b-col cols="12" lg="4">
+                  <h2 class="mb-0">Lebih Dekat Dengan Kami</h2>
+                  <p>Anda akan mendapat kabar lebih banyak tentang kami</p>
+                </b-col>
+                <b-col cols="12" lg="4">
+                  <b-form-input placeholder="Nama"></b-form-input>
+                  <b-form-input placeholder="Email" class="mt-2"></b-form-input>
+                  <b-button variant="outline-dark" class="mt-2 rounded-lg">Kirim</b-button>
+                </b-col>
+                <b-col cols="12" lg="4">
+                  <a href="#" class="d-block">0895-3771-64901</a>
+                  <a href="#" class="d-block">rotiity@gmail.com</a>
+                </b-col>
+              </b-row>
             </div>
           </div>
 
-          <div class="section footer mb-0" id="sectionFooter">
-            <div class="container">
-              <div class="row footer-row">
-                <div class="col-12 text-right">
-                  <a
-                    class="btn btn-circle btn-outline-semi-light footer-circle-button"
-                    @click="scrollTo('#home')"
-                    href="javascript:;"
-                  >
-                    <i class="simple-icon-arrow-up"></i>
-                  </a>
-                </div>
-                <div class="col-12 text-center footer-content">
-                  <a @click="scrollTo('#home')" href="javascript:;">
-                    <img class="footer-logo" alt="footer logo" src="/assets/logos/white-full.svg" />
-                  </a>
-                </div>
-              </div>
-            </div>
-            <div class="container copyright pt-5 pb-5">
-              <div class="row">
-                <div class="col-12"></div>
-                <div class="col-12 text-center">
-                  <p class="mb-0">2022 © Haunan.dev</p>
-                </div>
+          <div class="d-flex justify-content-end mb-5 mr-5">
+            <a href="" class="d-inline-flex align-items-center" data-aos="zoom-in">
+              <img src="/assets/img/landing-page/sample/wa-icon.png" alt="" style="z-index: 4;">
+              <div class="p-3" style="background-color: #73F063;margin-left: -4px;z-index: 3;border-radius: 0px 20px 20px 0px;">Klik disini untuk <br>Chat Penjual</div>
+            </a>
+          </div>
+          
+          <div class="mb-0 bg-primary py-4" id="sectionFooter">
+            <div class="container d-flex justify-content-center align-items-center">
+              <div class="d-flex align-items-center">
+                <img src="/assets/img/landing-page/sample/logo-footer.png" alt="">
+                <p class="mb-0 text-white ml-4">@ All rights reserved.2023</p>
               </div>
             </div>
           </div>
@@ -461,3 +491,12 @@ export default {
   },
 };
 </script>
+
+<style lang="scss" scoped>
+.hero-lg-image {
+  position: absolute;
+  height: 700px;
+  left: 0;
+  bottom: 0;
+}
+</style>
